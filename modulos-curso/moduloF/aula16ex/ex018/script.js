9b6e837fab1598273544328bb1dc80b8a1e6d1a6
phpereira2 <phpereirasilva2@gmail.com>
2024-08-08 22:43:31 -0300

    finish ex18

---
+++ modulos-curso/moduloF/aula16ex/ex018/script.js
@@ -0,0 +1,61 @@
+let txtnum = document.getElementById('formnum')
+let txtlist = document.getElementById('formlista')
+let resultado = document.getElementById('res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+} 
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(txtnum.value) && !inLista(txtnum.value, valores)) {
+        valores.push(Number(txtnum.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${txtnum.value} adicionado.`
+        txtlist.appendChild(item)
+        resultado.innerHTML = ''
+    } else {
+        alert('Valor inválido ou já encontrado na lista.')
+    }
+    txtnumvalue = ''
+    txtnum.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            } if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / total
+        resultado.innerHTML = ''
+        resultado.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+        resultado.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        resultado.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        resultado.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        resultado.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
+    }
+}
